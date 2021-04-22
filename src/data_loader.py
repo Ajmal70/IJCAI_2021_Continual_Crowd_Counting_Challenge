5ad74c0f5f97b291131c99a3f13a123365435bb0
Salman Khan <32925691+salmank255@users.noreply.github.com>
2021-04-22 18:01:34 +0100

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,11 +5,12 @@ import random
 import pandas as pd
 
 class ImageDataLoader():
-    def __init__(self, data_path, gt_path, shuffle=False, gt_downsample=False, pre_load=False):
+    def __init__(self, data_path, gt_path,split, shuffle=False, gt_downsample=False, pre_load=False):
         #pre_load: if true, all training and validation images are loaded into CPU RAM for faster processing.
         #          This avoids frequent file reads. Use this only for small datasets.
         self.data_path = data_path
         self.gt_path = gt_path
+        self.split = split
         self.gt_downsample = gt_downsample
         self.pre_load = pre_load
         self.data_files = [filename for filename in os.listdir(data_path) \
@@ -34,18 +35,22 @@ class ImageDataLoader():
                 wd_1 = (wd/4)*4
                 img = cv2.resize(img,(int(wd_1),int(ht_1)))
                 img = img.reshape((1,1,img.shape[0],img.shape[1]))
-                den = pd.read_csv(os.path.join(self.gt_path,os.path.splitext(fname)[0] + '.csv'), sep=',',header=None).to_numpy()                        
-                den  = den.astype(np.float32, copy=False)
-                if self.gt_downsample:
-                    wd_1 = wd_1/4
-                    ht_1 = ht_1/4
-                    den = cv2.resize(den,(int(wd_1),int(ht_1)))                
-                    den = den * ((wd*ht)/(wd_1*ht_1))
+                if self.split == 'train_split':
+                    den = pd.read_csv(os.path.join(self.gt_path,os.path.splitext(fname)[0] + '.csv'), sep=',',header=None).to_numpy()                        
+                    den  = den.astype(np.float32, copy=False)
+
+                    if self.gt_downsample:
+                        wd_1 = wd_1/4
+                        ht_1 = ht_1/4
+                        den = cv2.resize(den,(int(wd_1),int(ht_1)))                
+                        den = den * ((wd*ht)/(wd_1*ht_1))
+                    else:
+                        den = cv2.resize(den,(wd_1,ht_1))
+                        den = den * ((wd*ht)/(wd_1*ht_1))
+                        
+                    den = den.reshape((1,1,den.shape[0],den.shape[1]))
                 else:
-                    den = cv2.resize(den,(wd_1,ht_1))
-                    den = den * ((wd*ht)/(wd_1*ht_1))
-                    
-                den = den.reshape((1,1,den.shape[0],den.shape[1]))            
+                    den = []        
                 blob = {}
                 blob['data']=img
                 blob['gt_density']=den
@@ -81,18 +86,21 @@ class ImageDataLoader():
                 wd_1 = (wd/4)*4
                 img = cv2.resize(img,(wd_1,ht_1))
                 img = img.reshape((1,1,img.shape[0],img.shape[1]))
-                den = pd.read_csv(os.path.join(self.gt_path,os.path.splitext(fname)[0] + '.csv'), sep=',',header=None).as_matrix()                        
-                den  = den.astype(np.float32, copy=False)
-                if self.gt_downsample:
-                    wd_1 = wd_1/4
-                    ht_1 = ht_1/4
-                    den = cv2.resize(den,(wd_1,ht_1))                
-                    den = den * ((wd*ht)/(wd_1*ht_1))
+                if self.split == 'train_split':
+                    den = pd.read_csv(os.path.join(self.gt_path,os.path.splitext(fname)[0] + '.csv'), sep=',',header=None).as_matrix()                        
+                    den  = den.astype(np.float32, copy=False)
+                    if self.gt_downsample:
+                        wd_1 = wd_1/4
+                        ht_1 = ht_1/4
+                        den = cv2.resize(den,(wd_1,ht_1))                
+                        den = den * ((wd*ht)/(wd_1*ht_1))
+                    else:
+                        den = cv2.resize(den,(wd_1,ht_1))
+                        den = den * ((wd*ht)/(wd_1*ht_1))
+                        
+                    den = den.reshape((1,1,den.shape[0],den.shape[1]))
                 else:
-                    den = cv2.resize(den,(wd_1,ht_1))
-                    den = den * ((wd*ht)/(wd_1*ht_1))
-                    
-                den = den.reshape((1,1,den.shape[0],den.shape[1]))            
+                    den = []           
                 blob = {}
                 blob['data']=img
                 blob['gt_density']=den
