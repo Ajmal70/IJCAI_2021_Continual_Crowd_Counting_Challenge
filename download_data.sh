ef93c2794780e7559a8652d97449704407b7e1e6
Salman Khan <32925691+salmank255@users.noreply.github.com>
2021-05-06 12:26:08 +0100

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,6 +1,6 @@
 
 #Downloading contiguous videos in zip
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1phQi86FvLXBoOeh9jeZ_-MomUbcGW04K' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1phQi86FvLXBoOeh9jeZ_-MomUbcGW04K" -O data.zip
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1N71uZvw9Z3wqHRucJKmvmOwDWNIfK_1y' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1N71uZvw9Z3wqHRucJKmvmOwDWNIfK_1y" -O data.zip
 #Unzipping the videos
 unzip data.zip
 #Removing the zip file
